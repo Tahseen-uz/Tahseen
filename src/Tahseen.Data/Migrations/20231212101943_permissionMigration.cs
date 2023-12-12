ca0e39544f662b584ca501b5cc80030bb6057074
Sardorbek9343 <tolibjonovsardorbek12@gmail.com>
2023-12-12 15:27:34 +0500

    BookRnetalPermission entity added

---
+++ src/Tahseen.Data/Migrations/20231212101943_permissionMigration.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Tahseen.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class permissionMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookRentalPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    BookId = table.Column<long>(type: "bigint", nullable: false),
+                    LibraryBranchId = table.Column<long>(type: "bigint", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookRentalPermissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookRentalPermissions_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookRentalPermissions_LibraryBranches_LibraryBranchId",
+                        column: x => x.LibraryBranchId,
+                        principalTable: "LibraryBranches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookRentalPermissions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookRentalPermissions_BookId",
+                table: "BookRentalPermissions",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookRentalPermissions_LibraryBranchId",
+                table: "BookRentalPermissions",
+                column: "LibraryBranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookRentalPermissions_UserId",
+                table: "BookRentalPermissions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookRentalPermissions");
+        }
+    }
+}
--- src/Tahseen.Service/Mappings/MappingProfile.cs
+++ src/Tahseen.Service/Mappings/MappingProfile.cs
@@ -5,6 +5,7 @@ using Tahseen.Domain.Entities.Books;
 using Tahseen.Domain.Entities.EBooks;
 using Tahseen.Domain.Entities.Events;
 using Tahseen.Domain.Entities.Feedbacks;
+using Tahseen.Domain.Entities.Languages;
 using Tahseen.Domain.Entities.Librarians;
 using Tahseen.Domain.Entities.Library;
 using Tahseen.Domain.Entities.Narrators;
@@ -17,6 +18,7 @@ using Tahseen.Service.DTOs.AudioBooks.AudioBook;
 using Tahseen.Service.DTOs.AudioBooks.AudioFile;
 using Tahseen.Service.DTOs.Books.Author;
 using Tahseen.Service.DTOs.Books.Book;
+using Tahseen.Service.DTOs.Books.BookRentalPermission;
 using Tahseen.Service.DTOs.Books.BookReviews;
 using Tahseen.Service.DTOs.Books.CompletedBooks;
 using Tahseen.Service.DTOs.Books.Genre;
@@ -31,10 +33,11 @@ using Tahseen.Service.DTOs.Feedbacks.SurveyResponses;
 using Tahseen.Service.DTOs.Feedbacks.Surveys;
 using Tahseen.Service.DTOs.Feedbacks.UserMessages;
 using Tahseen.Service.DTOs.Feedbacks.UserRatings;
+using Tahseen.Service.DTOs.Languages;
 using Tahseen.Service.DTOs.Librarians;
 using Tahseen.Service.DTOs.Libraries.LibraryBranch;
-using Tahseen.Service.DTOs.Notifications;
 using Tahseen.Service.DTOs.Narrators;
+using Tahseen.Service.DTOs.Notifications;
 using Tahseen.Service.DTOs.Reservations;
 using Tahseen.Service.DTOs.Rewards.Badge;
 using Tahseen.Service.DTOs.Rewards.UserBadges;
@@ -48,29 +51,27 @@ using Tahseen.Service.DTOs.Users.UserCart;
 using Tahseen.Service.DTOs.Users.UserProgressTracking;
 using Tahseen.Service.DTOs.Users.UserSettings;
 using Tahseen.Service.DTOs.Users.Wishlists;
-using Tahseen.Domain.Entities.Languages;
-using Tahseen.Service.DTOs.Languages;
 
 namespace Tahseen.Service.Mappings;
 
-public class MappingProfile:Profile
+public class MappingProfile : Profile
 {
     public MappingProfile()
     {
 
         //Folder Name: Books
         CreateMap<BookReviews, BookReviewForCreationDto>().ReverseMap();
-        CreateMap<BookReviews,BookReviewForResultDto>().ReverseMap();
+        CreateMap<BookReviews, BookReviewForResultDto>().ReverseMap();
         CreateMap<BookReviews, BookReviewForUpdateDto>().ReverseMap();
 
         CreateMap<CompletedBooks, CompletedBookForCreationDto>().ReverseMap();
-        CreateMap<CompletedBooks,CompletedBookForResultDto>().ReverseMap();
-        CreateMap<CompletedBooks,CompletedBookForUpdateDto>().ReverseMap();
+        CreateMap<CompletedBooks, CompletedBookForResultDto>().ReverseMap();
+        CreateMap<CompletedBooks, CompletedBookForUpdateDto>().ReverseMap();
 
-        CreateMap<Publisher,PublisherForCreationDto>().ReverseMap();
-        CreateMap<Publisher,PublisherForResultDto>().ReverseMap();
-        CreateMap<Publisher,PublisherForUpdateDto>().ReverseMap();
-        
+        CreateMap<Publisher, PublisherForCreationDto>().ReverseMap();
+        CreateMap<Publisher, PublisherForResultDto>().ReverseMap();
+        CreateMap<Publisher, PublisherForUpdateDto>().ReverseMap();
+
         CreateMap<Author, AuthorForCreationDto>().ReverseMap();
         CreateMap<Author, AuthorForUpdateDto>().ReverseMap();
         CreateMap<Author, AuthorForResultDto>().ForMember(e => e.Nationality, o => o.MapFrom(src => src.Nationality.ToString())).ReverseMap();
@@ -83,32 +84,35 @@ public class MappingProfile:Profile
         CreateMap<Genre, GenreForUpdateDto>().ReverseMap();
         CreateMap<Genre, GenreForResultDto>().ReverseMap();
 
+        CreateMap<BookRentalPermission, BookRentalPermissionForCreationDto>().ReverseMap();
+        CreateMap<BookRentalPermission, BookRentalPermissionForResultDto>().ReverseMap();
+
         //Folder Name: Events
         CreateMap<Event, EventForCreationDto>().ReverseMap();
         CreateMap<Event, EventForUpdateDto>().ReverseMap(); //done
-        CreateMap<Event, EventForResultDto>().ForMember(e => e.Status, o => o.MapFrom(src => src.Status.ToString() )).ReverseMap();
+        CreateMap<Event, EventForResultDto>().ForMember(e => e.Status, o => o.MapFrom(src => src.Status.ToString())).ReverseMap();
 
         CreateMap<EventRegistration, EventRegistrationForCreationDto>().ReverseMap();
         CreateMap<EventRegistration, EventRegistrationForUpdateDto>().ReverseMap();
         CreateMap<EventRegistration, EventRegistrationForResultDto>().ForMember(e => e.Status, o => o.MapFrom(src => src.Status.ToString())).ReverseMap();
 
         //Folder Name: Feedbacks
-        CreateMap<Feedback,FeedbackForCreationDto>().ReverseMap();
-        CreateMap<Feedback,FeedbackForResultDto>().ReverseMap();
-        CreateMap<Feedback,FeedbackForUpdateDto>().ReverseMap();
-        
-        CreateMap<News,NewsForCreationDto>().ReverseMap();
-        CreateMap<News,NewsForResultDto>().ReverseMap();
+        CreateMap<Feedback, FeedbackForCreationDto>().ReverseMap();
+        CreateMap<Feedback, FeedbackForResultDto>().ReverseMap();
+        CreateMap<Feedback, FeedbackForUpdateDto>().ReverseMap();
+
+        CreateMap<News, NewsForCreationDto>().ReverseMap();
+        CreateMap<News, NewsForResultDto>().ReverseMap();
         CreateMap<News, NewsForUpdateDto>().ReverseMap();
-        
-        CreateMap<Surveys,SurveyForCreationDto>().ReverseMap();
-        CreateMap<Surveys,SurveyForResultDto>().ForMember(e => e.Status, o => o.MapFrom(src => src.Status.ToString())).ReverseMap();
-        CreateMap<Surveys,SurveyForUpdateDto>().ReverseMap();
-        
-        CreateMap<SurveyResponses,SurveyResponseForCreationDto>().ReverseMap();
-        CreateMap<SurveyResponses,SurveyResponseForResultDto>().ReverseMap();
-        CreateMap<SurveyResponses,SurveyResponseForUpdateDto>().ReverseMap();
-        
+
+        CreateMap<Surveys, SurveyForCreationDto>().ReverseMap();
+        CreateMap<Surveys, SurveyForResultDto>().ForMember(e => e.Status, o => o.MapFrom(src => src.Status.ToString())).ReverseMap();
+        CreateMap<Surveys, SurveyForUpdateDto>().ReverseMap();
+
+        CreateMap<SurveyResponses, SurveyResponseForCreationDto>().ReverseMap();
+        CreateMap<SurveyResponses, SurveyResponseForResultDto>().ReverseMap();
+        CreateMap<SurveyResponses, SurveyResponseForUpdateDto>().ReverseMap();
+
         CreateMap<UserMessage, UserMessageForCreationDto>().ReverseMap();
         CreateMap<UserMessage, UserMessageForResultDto>().ReverseMap();
         CreateMap<UserMessage, UserMessageForUpdateDto>().ReverseMap();
@@ -149,23 +153,23 @@ public class MappingProfile:Profile
         CreateMap<UserBadges, UserBadgesForResultDto>().ReverseMap();
 
         //Folder Name: SchoolAndEducations
-        CreateMap<PupilBookConnection,PupilBookConnectionForCreationDto>().ReverseMap();
-        CreateMap<PupilBookConnection,PupilBookConnectionForResultDto>().ReverseMap();
-        CreateMap<PupilBookConnection,PupilBookConnectionForUpdateDto>().ReverseMap();
-        
-        CreateMap<Pupil,PupilForCreationDto>().ReverseMap();
-        CreateMap<Pupil,PupilForResultDto>().ReverseMap();
-        CreateMap<Pupil,PupilForUpdateDto>().ReverseMap();
-        
-        CreateMap<SchoolBook,SchoolBookForCreationDto>().ReverseMap();
-        CreateMap<SchoolBook,SchoolBookForResultDto>().ReverseMap();
-        CreateMap<SchoolBook,SchoolBookForUpdateDto>().ReverseMap();
+        CreateMap<PupilBookConnection, PupilBookConnectionForCreationDto>().ReverseMap();
+        CreateMap<PupilBookConnection, PupilBookConnectionForResultDto>().ReverseMap();
+        CreateMap<PupilBookConnection, PupilBookConnectionForUpdateDto>().ReverseMap();
+
+        CreateMap<Pupil, PupilForCreationDto>().ReverseMap();
+        CreateMap<Pupil, PupilForResultDto>().ReverseMap();
+        CreateMap<Pupil, PupilForUpdateDto>().ReverseMap();
+
+        CreateMap<SchoolBook, SchoolBookForCreationDto>().ReverseMap();
+        CreateMap<SchoolBook, SchoolBookForResultDto>().ReverseMap();
+        CreateMap<SchoolBook, SchoolBookForUpdateDto>().ReverseMap();
 
 
 
         //Folder Name: Users
         CreateMap<User, UserForCreationDto>().ReverseMap();
-        CreateMap<User, UserForResultDto>().ForMember(r => r.Roles, opt => opt.MapFrom(src => src.Role.ToString()) ).ReverseMap();
+        CreateMap<User, UserForResultDto>().ForMember(r => r.Roles, opt => opt.MapFrom(src => src.Role.ToString())).ReverseMap();
         CreateMap<User, UserForUpdateDto>().ReverseMap();
 
         CreateMap<UserCart, UserCartForCreationDto>().ReverseMap();
