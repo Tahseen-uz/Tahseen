2619f53893d6dc17f232a57bb1512369a7f16ce1
Sardorbek9343 <s.tolibjonov@tuit.uz>
2023-12-21 15:32:15 +0500

    Done

---
+++ src/Tahseen.Service/DTOs/Books/BookComplatePermissions/BookComplatePermissionForResultDto.cs
@@ -0,0 +1,15 @@
+﻿using Tahseen.Domain.Entities.Library;
+using Tahseen.Domain.Entities;
+using Tahseen.Service.DTOs.Users.User;
+using Tahseen.Service.DTOs.Books.Book;
+using Tahseen.Service.DTOs.Libraries.LibraryBranch;
+
+namespace Tahseen.Service.DTOs.Books.BookComplatePermissions;
+
+public class BookComplatePermissionForResultDto
+{
+    public UserForResultDto User { get; set; }
+    public BookForResultDto Book { get; set; }
+    public LibraryBranchForResultDto LibraryBranch { get; set; }
+    public string Comment { get; set; }
+}
